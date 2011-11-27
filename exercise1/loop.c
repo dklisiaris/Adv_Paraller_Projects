05f2ff05c82e365de0f6bf874d28d6a45a713b70
Mits Klisiaris <meec@ubuntu.(none)>
2011-11-27 13:01:34 +0200

    basic implemention of pthread execute loop completed

--- exercise1/loop.c
+++ exercise1/loop.c
@@ -1,24 +1,75 @@
 #include "loop.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
 
-int pthread_execute_loop(void (*func)(int low, int high),int low, int high)
+
+int pthread_execute_loop(void (*func)(void *arg, int low, int high),void *arg, int nthreads, int low, int high)
 {
-/*	void (*fp)(int,int);
+	if(nthreads<1)return EINVAL;
 
-	fp=func;
-	fp(2,5);
-	(*fp)(3,9);*/
-	func(low,high);
+	pthread_data * pt_data;	
+
+	/*allocate some memory for the thread structs*/
+	pt_data=malloc(nthreads * (sizeof(pthread_data)));
+	if(pt_data==NULL)
+	{
+		fprintf(stderr, "out of memory\n");
+		exit(1);
+	}
+
+	set_up_pthread_data(func, arg, pt_data, nthreads, low, high);
+	pthread_t thread[nthreads];//definition of threads
+
+	int i,j;
+	/*create the #nthreads threads with  and pt_info struct as argument*/
+	for(i=0;i<nthreads;i++)
+	{
+		pthread_create(&thread[i],NULL, (void*) &stat_pfunc,(void*)&pt_data[i]);
+	}
+	
+	/*join threads when they are finished*/
+	for(j=0;j<nthreads;j++)
+	{
+		pthread_join(thread[j], NULL);
+	}
+
+	//func(low,high);
 	return 0;
 }
 
-void loop_func(int start, int stop)
+void set_up_pthread_data(void(*func)(void *arg,int low,int high),void *arg, pthread_data *pd, int nthreads, int init_low, int init_high)
 {
-	int i;
-	for(i=start;i<=stop;i++)
+	//Number of total iterations
+	int N = init_high-init_low+1;
+	//Number of iterations per thread
+	int range = N/nthreads;
+	//Extra iterations to be executed in case N%nthreads!=0 
+	int extra_iters = N%nthreads;
+	int i, temp_low=init_low,extra_flag=0;
+
+	for(i=0;i<nthreads;i++)
 	{
-		printf("i:%d\n",i);	
+		if(extra_iters!=0)extra_flag=1;
+		pd[i].low=temp_low;
+		pd[i].high=temp_low+range-1+extra_flag;
+		
+		temp_low+=(range+extra_flag);
+		if(extra_iters!=0)extra_iters--;
+		extra_flag=0;
+
+		pd[i].func=func;
+		pd[i].f_arg=arg;
 	}
+}
+
+void stat_pfunc(void * arg)
+{
+	pthread_data * pd = (pthread_data *)arg;
+	void (*func)(void*,int,int);
+	func=pd->func;
+	func(pd->f_arg, pd->low, pd->high);
 
 }
+
--- exercise1/loop.h
+++ exercise1/loop.h
@@ -1,3 +1,19 @@
-int pthread_execute_loop(void (*func)(int low, int high),int low, int high);
+#ifndef LOOP_H
+#define LOOP_H
 
-void loop_func(int start, int stop);
+typedef struct
+{
+	int low;
+	int high;
+	void (*func)(void *arg, int low, int high);
+	void * f_arg;
+
+}pthread_data;
+
+int pthread_execute_loop(void (*func)(void *arg, int low, int high),void *arg, int nthreads, int low, int high);
+
+void set_up_pthread_data(void(*func)(void *arg,int low,int high),void *arg, pthread_data *pd, int nthreads, int init_low, int init_high);
+
+void stat_pfunc(void * arg);
+
+#endif
